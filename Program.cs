aa3961dbdf558b1fcd8764b76dc0bc1567cc0ea5
Phil Moir <pmoir@dctfh.com>
2015-04-29 23:58:19 +0100

    Simple console app to test the APIs of YouTrack and Asana exports and Trello import

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtractIssuesFromAsana
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var asanaKey = "61rP9kdl.kK5NxMzVClNyLdPYmSbRrgx:";
+            //var asanaKeyEncoded = "NjFyUDlrZGwua0s1TnhNelZDbE55TGRQWW1TYlJyZ3g6";
+
+            //var httpClient = new HttpClient();
+            ////var url = "https://app.asana.com/api/1.0/tasks/27143324952195?opt_fields=name,notes";
+            //var url = "https://app.asana.com/api/1.0/projects";
+            //var request = new HttpRequestMessage(HttpMethod.Get, url);
+            //AddAsanaHeaders(request);
+            //var response = httpClient.SendAsync(request).Result;
+            //response.EnsureSuccessStatusCode();
+            //var result = response.Content.ReadAsStringAsync().Result;
+            //Console.WriteLine(result);
+            //Console.ReadKey();
+
+            var httpClient = new HttpClient();
+            var url = "http://youtrack.brightsolid.com/rest/admin/permission";
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+            AddYouTrackHeaders(request);
+            var response = httpClient.SendAsync(request).Result;
+            response.EnsureSuccessStatusCode();
+            var result = response.Content.ReadAsStringAsync().Result;
+            var oMycustomclassname = Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(result);
+            foreach (dynamic o in oMycustomclassname)
+            {
+                Console.WriteLine(o.name);
+            }
+            Console.WriteLine(result);
+            Console.ReadKey();
+        
+        }
+
+        static void AddAsanaHeaders(HttpRequestMessage request)
+        {
+            request.Headers.Add("Authorization", "Basic NjFyUDlrZGwua0s1TnhNelZDbE55TGRQWW1TYlJyZ3g6");
+        }
+
+        static void AddYouTrackHeaders(HttpRequestMessage request)
+        {
+            request.Headers.Add("Accept", "application/json");
+        }
+    }
+}
